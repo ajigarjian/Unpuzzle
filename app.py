ce4ad4f9f6d82325b29416e57968f09915f95c80
Ari Jigarjian <ajigarjian@gmail.com>
2022-02-02 00:41:14 -0500

    Fourth commit

--- app.py
+++ app.py
@@ -8,13 +8,16 @@ app = Flask(__name__)
 
 # Configure CS50 Library to use SQLite database
 db = sqlite3.connect('pairs.db', check_same_thread=False)
+cursor = db.cursor()
 
 @app.route("/")
 def index():
     """Show home page of UnPuzzle"""
 
-    rows = db.execute("SELECT answer FROM general LIMIT 10")
-    rows = rows.fetchall()
+
+    rows = []
+    for row in cursor.execute("SELECT * FROM general"):
+        rows.append(row)
     return render_template("index.html", rows=rows)
 
 if __name__ == "__main__":
