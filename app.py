fe1681dc934c7f4aa10ae165bec11b52ba8a53e6
Ari Jigarjian <ajigarjian@gmail.com>
2022-02-02 18:40:41 -0500

    Changed name to 1Across and added javascript file for search/sort logic

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
-import os
-
+# importing necessary frameworks and libraries 
 from flask import Flask, render_template
 import sqlite3
 
 # Configure application
 app = Flask(__name__)
 
-# Configure CS50 Library to use SQLite database
+# Connect to SQLite database pairs.db and initialize cursor based on connection
 db = sqlite3.connect('pairs.db', check_same_thread=False)
 cursor = db.cursor()
 
+# Create route for logic behind home page. Fills list "rows" with rows from general table, and renders index.html template with "rows" as input
 @app.route("/")
 def index():
-    """Show home page of UnPuzzle"""
-
-
+    """Show home page of 1Across"""
     rows = []
     for row in cursor.execute("SELECT * FROM general LIMIT 100"):
         rows.append(row)
     return render_template("index.html", rows=rows)
 
+#syntax to run app.py
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,6 @@
+// Creates a function that, upon click, changes the color of the element with the id "one_header" to red
+document.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("one_header").style.color = "red";
+}
