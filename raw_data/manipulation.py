1c1eae4d2cc81ddca6f835187727fdac7b025238
Ari Jigarjian <ajigarjian@gmail.com>
2022-02-17 18:59:31 -0500

    change

--- raw_data/manipulation.py
+++ raw_data/manipulation.py
@@ -3,6 +3,9 @@ import csv
 f = open('xword (1).csv')
 csv_f = csv.reader(f)
 
-for i, v in enumerate(csv_f):
-    if i < 10:
-        print(v)
+with open('db.csv', mode='w') as db_file:
+    db_writer = csv.writer(db_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+for row in csv_f:
+    db_writer.writerow(row)
+
